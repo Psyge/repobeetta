7c27f6d17e2062422cd124e051ad1b6630a60f28
Psyge <masto84@gmail.com>
2025-11-11 19:40:35 +0200

    Create markers.js

---
+++ markers.js
@@ -0,0 +1,27 @@
+const places = [
+  { name: 'Rovaniemi', lat: 66.5, lon: 25.7, url: 'https://visitrovaniemi.fi', icon: 'roic.png' },
+  
+];
+
+
+const markersLayer = L.layerGroup();
+
+function addMarkers() {
+  places.forEach(place => {
+    const customIcon = L.icon({
+      iconUrl: place.icon,
+      iconSize: [32, 32],
+      iconAnchor: [16, 32],
+      popupAnchor: [0, -32]
+    });
+
+    const popupContent = `
+      <strong>${place.name}</strong><br>
+      ${place.url}More info</a>
+    `;
+
+    L.marker([place.lat, place.lon], { icon: customIcon })
+      .bindPopup(popupContent)
+      .addTo(markersLayer);
+  });
+}
