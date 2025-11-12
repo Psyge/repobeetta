90f86f8117d3f886f365c2f270d31c7d90faa6e0
Psyge <masto84@gmail.com>
2025-11-12 18:54:06 +0200

    Update markers.js

--- markers.js
+++ markers.js
@@ -1,6 +1,8 @@
 const places = [
   { name: 'Rovaniemi', lat: 66.5, lon: 25.7, url: 'https://visitrovaniemi.fi', icon: 'roic.png' },
-  
+  { name: 'Joulupukin Pajakylä', lat: 66.54, lon: 25.84, url: 'https://santaclausvillage.info/', icon: 'pukki.png' },
+  { name: 'Levi', lat: 67.80, lon: 24.81, url: 'https://www.levi.fi/', icon: 'levi.png' },
+  { name: 'Ylläs', lat: 57.56, lon: 24.22, url: 'https://yllas.fi/', icon: 'yllas.png' },
 ];
 
 
