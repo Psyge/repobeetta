d0a8e8ee121c4e750458f235b1fa9b5e309bc58f
Psyge <masto84@gmail.com>
2025-11-13 20:30:38 +0200

    Update markers.js

--- markers.js
+++ markers.js
@@ -5,35 +5,21 @@ const places = [
   { name: 'Ylläs', lat: 67.57, lon: 24.20, url: 'https://yllas.fi/', icon: 'yllas.png' }
 ];
 
-const markersLayer = L.layerGroup();
-markersLayer.addTo(map); // Varmista, että 'map' on jo luotu
+const markersLayer = L.layerGroup().addTo(map);
 
 function addMarkers() {
   places.forEach(place => {
-    const customIcon = L.divIcon({
-      className: 'drop-icon',
-      html: `
-       
- <svg width="50" height="70" viewBox="0 0 50 70" xmlns="http://www.w3.org/2000/svg">
-      <!-- Sininen pisara -->
-      <path d="M25 0 C40 20, 40 45, 25 70 C10 45, 10 20, 25 0" fill="#007bff" stroke="#0056b3" stroke-width="2"/>
-      
-      <!-- Valkoinen ympyrä -->
-      <circle cx="25" cy="20" r="14" fill="white" stroke="#0056b3" stroke-width="2"/>
-      
-      <!-- Paikan ikoni ympyrän sisään -->
-      <image href="${place.icon}" x="11" y="6" width="28" height="28" clip-path="circle(14px at 25px 20px)"/>
-    </svg>
-
-      `,
-      iconAnchor: [25, 70],
+    const customIcon = L.icon({
+      iconUrl: 'pinni.png', // tämä on pisaran muotoinen pinni
+      iconSize: [50, 70],        // sama kuin kuvan koko
+      iconAnchor: [25, 70],      // kärki osoittaa oikeaan paikkaan
       popupAnchor: [0, -70]
     });
 
     const popupContent = `
       <strong>${place.name}</strong><br>
-      <img src="${place.icon}" alt="${place.name}" class="popup-img"><br>
-      <a href="${place.url}" target="_blank" rel="noopener noreferrer">More info</a>
+      <img src="${place.icon}" alt="${place.name}" style="width:50px;height:50px;border-radius:50%;"><br>
+      <a href="${place.url}" target="_blank">More info</a>
     `;
 
     L.marker([place.lat, place.lon], { icon: customIcon })
@@ -41,4 +27,5 @@ function addMarkers() {
       .addTo(markersLayer);
   });
 }
+
 addMarkers();
