38076ff3e6d0e106d912c2d713b8ea3858908b2f
Psyge <masto84@gmail.com>
2025-11-13 20:02:20 +0200

    Update markers.js

--- markers.js
+++ markers.js
@@ -4,28 +4,25 @@ const places = [
   { name: 'Levi', lat: 67.80, lon: 24.80, url: 'https://www.levi.fi/', icon: 'levi.png' },
   { name: 'Ylläs', lat: 67.57, lon: 24.20, url: 'https://yllas.fi/', icon: 'yllas.png' }
 ];
-    
 
-const customIcon = L.divIcon({
-  className: 'drop-icon',
-  html: `
-    <svg width="32" height="40" viewBox="0 0 32 40">
-      <path d="M16 0 C26 15, 26 30, 16 40 C6 30, 6 15, 16 0" fill="#00aaff">
-        <animateTransform attributeName="transform" type="translate" values="0,0; 0,-5; 0,0" dur="3s" repeatCount="indefinite"/>
-      </path>
-    </svg>
-  `,
-  iconAnchor: [16, 40],
-  popupAnchor: [0, -40]
-});
-
-
-
-    
-
+const markersLayer = L.layerGroup();
+markersLayer.addTo(map); // Varmista, että 'map' on jo luotu
 
 function addMarkers() {
   places.forEach(place => {
+    const customIcon = L.divIcon({
+      className: 'drop-icon',
+      html: `
+        <svg width="32" height="40" viewBox="0 0 32 40">
+          <path d="M16 0 C26 15, 26 30, 16 40 C6 30, 6 15, 16 0" fill="#00aaff">
+            <animateTransform attributeName="transform" type="translate" values="0,0; 0,-5; 0,0" dur="3s" repeatCount="indefinite"/>
+          </path>
+        </svg>
+      `,
+      iconAnchor: [16, 40],
+      popupAnchor: [0, -40]
+    });
+
     const popupContent = `
       <strong>${place.name}</strong><br>
       <img src="${place.icon}" alt="${place.name}" class="popup-img"><br>
@@ -38,8 +35,4 @@ function addMarkers() {
   });
 }
 
-
-
-    
-
-
+addMarkers();
