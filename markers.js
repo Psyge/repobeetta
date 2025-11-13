2b7f78e4bc1c1783a22215fcbce6e598298e7666
Psyge <masto84@gmail.com>
2025-11-13 19:55:38 +0200

    Update markers.js

--- markers.js
+++ markers.js
@@ -38,16 +38,4 @@ const popupContent = `
 
     
 
-const popupContent = `
-  <strong>${place.name}</strong><br>
-  <img src="${place.icon}" alt="${place.name}" class="popup-img"><br>
-  <a href="${place.url}" target="_blank" rel="noopener noreferrer">More info</a>
-`;
 
-
-
-    L.marker([place.lat, place.lon], { icon: customIcon })
-      .bindPopup(popupContent)
-      .addTo(markersLayer);
-  });
-}
