0e014df35b8f4e30fcd604727cd8f725e5571e7f
Psyge <masto84@gmail.com>
2025-11-13 19:55:11 +0200

    Update markers.js

--- markers.js
+++ markers.js
@@ -4,24 +4,6 @@ const places = [
   { name: 'Levi', lat: 67.80, lon: 24.80, url: 'https://www.levi.fi/', icon: 'levi.png' },
   { name: 'Ylläs', lat: 67.57, lon: 24.20, url: 'https://yllas.fi/', icon: 'yllas.png' }
 ];
-
-
-const markersLayer = L.layerGroup();
-
-function addMarkers() {
-  places.forEach(place => {
-   const places = [
-  { name: 'Rovaniemi', lat: 66.5, lon: 25.7, url: 'https://visitrovaniemi.fi', icon: 'roic.png' },
-  { name: 'Joulupukin Pajakylä', lat: 66.54, lon: 25.84, url: 'https://santaclausvillage.info/', icon: 'pukki.png' },
-  { name: 'Levi', lat: 67.80, lon: 24.80, url: 'https://www.levi.fi/', icon: 'levi.png' },
-  { name: 'Ylläs', lat: 67.57, lon: 24.20, url: 'https://yllas.fi/', icon: 'yllas.png' }
-];
-
-
-const markersLayer = L.layerGroup();
-
-function addMarkers() {
-  places.forEach(place => {
     
 const customIcon = L.divIcon({
   className: 'drop-icon',
