6141a1b5dd1dde8ffe94f271a8784aa4fde8e5b6
Psyge <masto84@gmail.com>
2025-11-14 10:17:48 +0200

    Update markers.js

--- markers.js
+++ markers.js
@@ -1,6 +1,6 @@
 const places = [
   { name: 'Rovaniemi', lat: 66.5, lon: 25.7, url: 'https://visitrovaniemi.fi', icon: 'roic.png' },
-  { name: 'Joulupukin Pajakylä', lat: 66.54, lon: 25.84, url: 'https://santaclausvillage.info/', icon: 'pukki.png' },
+  { name: 'Joulupukin Pajakylä', lat: 66.54, lon: 25.84, url: 'https://santaclausvillage.info/', icon: 'pukki.png' stream: 'https://www.youtube.com/watch?v=Cp4RRAEgpeU'},
   { name: 'Levi', lat: 67.80, lon: 24.80, url: 'https://www.levi.fi/', icon: 'levi.png' },
   { name: 'Ylläs', lat: 67.57, lon: 24.20, url: 'https://yllas.fi/', icon: 'yllas.png' }
 ];
@@ -8,35 +8,48 @@ const places = [
 const markersLayer = L.layerGroup().addTo(map);
 
 function addMarkers() {
-  places.forEach(place => {
-    const customIcon = L.divIcon({
-      className: 'custom-marker',
-      html: `
-        <div class="marker-wrapper">
-          <img src="pinni.png" class="pin">
-          <img src="${place.icon}" class="pin-icon">
-        </div>
-      `,
-      iconSize: [32, 48],
-      iconAnchor: [16, 48],
-      popupAnchor: [0, -48]
+    places.forEach(place => {
+      const customIcon = L.divIcon({
+        className: 'custom-marker',
+        html: `
+          <div class="marker-wrapper">
+            <img src="pinni.png" class="pin">
+            <img src="${place.icon}" class="pin-icon">
+          </div>
+        `,
+        iconSize: [32, 48],
+        iconAnchor: [16, 48],
+        popupAnchor: [0, -48]
+      });
+
+      // Popupin perussisältö
+      const popupContent = `
+        <strong>${place.name}</strong><br>
+        <img src="${place.icon}" alt="${place.name}" style="width:50px;height:50px;border-radius:50%;"><br>
+        <a href="${place.url}" target="_blank">More info</a>
+        ${place.stream ? `<div class="popup-stream" data-stream="${place.stream}" style="margin-top:10px;"></div>` : ''}
+      `;
+
+      const marker = L.marker([place.lat, place.lon], { icon: customIcon })
+        .bindPopup(popupContent, { maxWidth: 320 })
+        .addTo(markersLayer);
+
+      // Lazy load stream iframe kun popup avataan
+      marker.on('popupopen', (e) => {
+        const container = e.popup.getElement().querySelector('.popup-stream');
+        if (container && !container.querySelector('iframe')) {
+          const iframe = document.createElement('iframe');
+          iframe.src = container.dataset.stream;
+          container.appendChild(iframe);
+        }
+      });
     });
 
-    const popupContent = `
-      <strong>${place.name}</strong><br>
-      <img src="${place.icon}" alt="${place.name}" style="width:50px;height:50px;border-radius:50%;"><br>
-      <a href="${place.url}" target="_blank">More info</a>
-    `;
+    // Satunnainen animaatioviive markereille
+    document.querySelectorAll('.marker-wrapper').forEach(el => {
+      el.style.animationDelay = `${Math.random() * 2}s`;
+    });
+  }
 
-    L.marker([place.lat, place.lon], { icon: customIcon })
-      .bindPopup(popupContent)
-      .addTo(markersLayer);
-  });
+  addMarkers();
 
-  // Nyt markerit ovat DOMissa → lisätään satunnainen viive
-  document.querySelectorAll('.marker-wrapper').forEach(el => {
-    el.style.animationDelay = `${Math.random() * 2}s`;
-  });
-}
-
-addMarkers();
