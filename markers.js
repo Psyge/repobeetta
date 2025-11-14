d4df747554aa6e13a9aebba188fd6f56e21a096b
Psyge <masto84@gmail.com>
2025-11-14 10:41:31 +0200

    Update markers.js

--- markers.js
+++ markers.js
@@ -8,50 +8,57 @@ const places = [
 const markersLayer = L.layerGroup().addTo(map);
 
 function addMarkers() {
-    places.forEach(place => {
-      const customIcon = L.divIcon({
-        className: 'custom-marker',
-        html: `
-          <div class="marker-wrapper">
-            <img src="pinni.png" class="pin">
-            <img src="${place.icon}" class="pin-icon">
-          </div>
-        `,
-        iconSize: [32, 48],
-        iconAnchor: [16, 48],
-        popupAnchor: [0, -48]
-      });
-
-      // Popupin perussisältö
-     const popupContent = `
-  <strong>${place.name}</strong><br>
-  <img src="${place.icon}" alt="${place.name}" style="width:50px;height:50px;border-radius:50%;"><br>
-  <a href="${place.url}" target="_blank">More info</a>
-  ${place.stream ? `<div class="popup-stream" 
-    data-stream="${place.stream}" 
-    data-width="${place.streamWidth || 320}" 
-    data-height="${place.streamHeight || 180}" 
-    style="margin-top:10px;"></div>` : ''}
-`;
-
-marker.on('popupopen', (e) => {
-  const container = e.popup.getElement().querySelector('.popup-stream');
-  if (container && !container.querySelector('iframe')) {
-    const iframe = document.createElement('iframe');
-    iframe.src = container.dataset.stream;
-    iframe.width = container.dataset.width;
-    iframe.height = container.dataset.height;
-    iframe.style.border = 'none';
-    iframe.style.display = 'block';
-    container.appendChild(iframe);
-  }
-});
-
-    // Satunnainen animaatioviive markereille
-    document.querySelectorAll('.marker-wrapper').forEach(el => {
-      el.style.animationDelay = `${Math.random() * 2}s`;
+  places.forEach(place => {
+    const customIcon = L.divIcon({
+      className: 'custom-marker',
+      html: `
+        <div class="marker-wrapper">
+          <img src="pinni.png" class="pin">
+          <img src="${place.icon}" class="pin-icon">
+        </div>
+      `,
+      iconSize: [32, 48],
+      iconAnchor: [16, 48],
+      popupAnchor: [0, -48]
     });
-  }
 
-  addMarkers();
+    // Popupin perussisältö
+    const popupContent = `
+      <strong>${place.name}</strong><br>
+      <img src="${place.icon}" alt="${place.name}" style="width:50px;height:50px;border-radius:50%;"><br>
+      <a href="${place.url}" target="_blank">More info</a>
+      ${place.stream ? `<div class="popup-stream" 
+        data-stream="${place.stream}" 
+        data-width="${place.streamWidth || 320}" 
+        data-height="${place.streamHeight || 180}" 
+        style="margin-top:10px;"></div>` : ''}
+    `;
+
+    // Luo marker ja lisää popup
+    const marker = L.marker([place.lat, place.lon], { icon: customIcon })
+      .bindPopup(popupContent, { className: 'custom-popup' })
+      .addTo(markersLayer);
+
+    // Lazy load iframe kun popup avataan
+    marker.on('popupopen', (e) => {
+      const container = e.popup.getElement().querySelector('.popup-stream');
+      if (container && !container.querySelector('iframe')) {
+        const iframe = document.createElement('iframe');
+        iframe.src = container.dataset.stream;
+        iframe.width = container.dataset.width;
+        iframe.height = container.dataset.height;
+        iframe.style.border = 'none';
+        iframe.style.display = 'block';
+        container.appendChild(iframe);
+      }
+    });
+  });
+
+  // Satunnainen animaatioviive markereille
+  document.querySelectorAll('.marker-wrapper').forEach(el => {
+    el.style.animationDelay = `${Math.random() * 2}s`;
+  });
+}
+
+addMarkers();
 
