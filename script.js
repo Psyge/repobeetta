0d9a37b5c34bc5e7dfb1c9afbaf57b729dd0c66a
Psyge <masto84@gmail.com>
2026-01-04 00:42:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -163,7 +163,7 @@ async function initAppMap() {
 
   L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
     noWrap: false, // Sallii maailman toistumisen
-    bounds: [[-90, -180], [90, 180]],
+    bounds: [[-90, -210], [90, 210]],
     maxZoom: 17
 }).addTo(map);
 
@@ -177,7 +177,7 @@ async function initAppMap() {
   const auroraLayerInstance = new AuroraLayer();
   map.addLayer(auroraLayerInstance);
 
-  map.setMaxBounds([[-90, -210], [90, 210]]);
+  map.setMaxBounds([[-90, -180], [90, 180]]);
   map.on('click', onMapClick);
 
   const places = await loadPlaces();
